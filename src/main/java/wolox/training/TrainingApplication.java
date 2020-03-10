0380a6ec78a84442dd43f0873c41e13e4c256c03
Federico Di Nucci <dinu1389@gmail.com>
2020-03-10 20:48:45 -0300

    Book Repository creation with search by author method

--- src/main/java/wolox/training/TrainingApplication.java
+++ src/main/java/wolox/training/TrainingApplication.java
@@ -3,7 +3,9 @@ package wolox.training;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "wolox.training.repositories")
 @EntityScan("wolox.training.models")
 @SpringBootApplication
 public class TrainingApplication {
