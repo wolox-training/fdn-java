b17e0e02c9daf2fb57c1088b6fff32578718095d
Federico Di Nucci <dinu1389@gmail.com>
2020-03-11 16:43:03 -0300

    Add Lombok to project and apply it to Book model

--- src/main/java/wolox/training/models/Book.java
+++ src/main/java/wolox/training/models/Book.java
@@ -5,123 +5,57 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@SequenceGenerator(name="BookSequence")
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BookSequence")
+    @Getter
     private long id;
 
+    @Getter @Setter
     @Column(nullable = false)
     private String title;
 
+    @Getter @Setter
     @Column(nullable = false)
     private String author;
 
+    @Getter @Setter
     @Column
     private String genre;
 
+    @Getter @Setter
     @Column(nullable = false)
     private String subtitle;
 
+    @Getter @Setter
     @Column(nullable = false)
     private String publisher;
 
+    @Getter @Setter
     @Column(nullable = false)
     private String year;
 
+    @Getter @Setter
     @Column(nullable = false)
     private int pages;
 
+    @Getter @Setter
     @Column(nullable = false)
     private String image;
 
+    @Getter @Setter
     @Column(nullable = false)
     private String isbn;
 
     public Book() {
 
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public String getSubtitle() {
-        return subtitle;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public int getPages() {
-        return pages;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public void setSubtitle(String subtitle) {
-        this.subtitle = subtitle;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-    public void setPages(int pages) {
-        this.pages = pages;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
     }
 }
 
