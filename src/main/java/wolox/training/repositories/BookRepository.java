736169c220d42c2dade67b8ad644a2a29c3f3064
Federico Di Nucci <dinu1389@gmail.com>
2020-03-11 17:27:33 -0300

    Add Javadoc to Book Repository interface

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -2,7 +2,11 @@ package wolox.training.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import wolox.training.models.Book;
-
+/**
+ * Repository Interface for {@link Book} class that extends {@link JpaRepository}
+ *
+ * @author federicodinucci
+**/
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Book findByAuthor(String author);
